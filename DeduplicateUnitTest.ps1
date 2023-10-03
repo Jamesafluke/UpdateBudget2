432fe2806e4fd064bf185310f0d2a0dafe11410b
James Fluckiger <jfluckiger@cloudbees.com>
2023-10-02 19:32:20 -0600

    Another iteration

--- DeduplicateUnitTest.ps1
+++
@@ -1,7 +0,0 @@
-. $PSScriptRoot\Modules\Deduplicate.ps1
-
-$existingBudgetData = Import-Csv $PSScriptRoot/UnitTests/existingBudgetData.csv
-
-Write-Host $existingBudgetData
-
-
---
+++ UnitTests/DeduplicateUnitTest.ps1
@@ -0,0 +1,9 @@
+. $PSScriptRoot\Deduplicate.ps1
+
+$existingBudgetData = Import-Csv $PSScriptRoot/existingBudgetData.csv
+
+Write-Host $existingBudgetData
+
+
+
+
