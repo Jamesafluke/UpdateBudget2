298b8425a7b03b4700a2e564f8a46fb86d5b27be
James Fluckiger <jfluckiger@cloudbees.com>
2023-10-03 08:17:33 -0600

    Another iteration

---
+++ UnitTests/ArbitraryExceptionsUnitTest/ArbitraryExceptionsUnitTest.ps1
@@ -0,0 +1,55 @@
+
+function Main{
+    . $PSScriptRoot\..\Modules\ArbitraryExceptions.ps1
+
+    $verifiedExpenses = Import-Csv $PSScriptRoot/UnitTests/ArbitraryExceptions/verifiedExpenses.csv
+
+    $verifiedExpenses = ArbitraryExceptions $verifiedExpenses
+
+    if(!(TestArbitraryExceptionAmericanFunds $verifiedExpenses)){
+        Write-Host "TestArbitraryExceptionAmericanFunds failed." -ForegroundColor Red
+    }else{
+        Write-Host "TestArbitraryExceptionAmericanFunds succeeded" -ForegroundColor Green
+    }
+}
+
+
+
+# Check if it returns arbitrary exceptions correctly.
+function TestArbitraryExceptionAmericanFunds {
+    param (
+        $verifiedExpenses
+    )
+    foreach($item in $verifiedExpenses){
+        if($item.Item -eq "American Funds" -and $item.Description -eq "This will become millions" -and $item.category -eq "Investment"){
+            return $true
+        }
+    }
+}
+function TestArbitraryExceptionHOA {
+    param (
+        $verifiedExpenses
+    )
+    foreach($item in $verifiedExpenses){
+        if($item.Item -eq "HOA" -and $item.category -eq "HOA"){
+            return $true
+        }
+    }
+}
+function TestArbitraryExceptionWalmart {
+    param (
+        $verifiedExpenses
+    )
+    $numberOfWalmart = 0
+    foreach($item in $verifiedExpenses){
+        if($item.Item -eq "Walmart" -and $item.category -eq "Groceries"){
+            $numberOfWalmart ++
+        }
+    }
+    if($numberOfWalmart -eq 2){
+        return $true
+    }
+    return $false
+}
+
+Main
