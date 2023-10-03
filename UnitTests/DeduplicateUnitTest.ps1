5ce2fdd3ac22bdc02667d850ce5448859d703799
James Fluckiger <jfluckiger@cloudbees.com>
2023-10-02 20:03:34 -0600

    Another iteration

--- UnitTests/DeduplicateUnitTest.ps1
+++ UnitTests/DeduplicateUnitTest.ps1
@@ -1,9 +1,16 @@
 . $PSScriptRoot\Deduplicate.ps1
 
-$existingBudgetData = Import-Csv $PSScriptRoot/existingBudgetData.csv
+$accountHistory = Import-Csv $PSScriptRoot/accountHistory.csv
+$existingBudget = Import-Csv $PSScriptRoot/existingBudget.csv
 
-Write-Host $existingBudgetData
+Write-Host $accountHistory
+Write-Host $existingBudget
 
 
+$result = Deduplicate $accountHistory $existingBudget
+
+if ($result.count -eq 6){
+    Write-Host "Count is correct." -ForegroundColor Green
+}
 
 
