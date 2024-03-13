074948bcbcbceecfc6bb883345a9a490e914ce82
James Fluckiger <jfluckiger@cloudbees.com>
2024-03-13 11:53:09 -0600

    Another iteration

--- UnitTests/ArbitraryExceptionsUnitTest/ArbitraryExceptionsUnitTest.ps1
+++ UnitTests/ArbitraryExceptionsUnitTest/ArbitraryExceptionsUnitTest.ps1
@@ -6,7 +6,7 @@ function Main{
     Write-Host "Inside ArbitraryExceptionsUnitTest.ps1 now!"
 
     . $PSScriptRoot\..\Modules\ArbitraryExceptions.ps1
-    pwsh Import-Module /home/runner/work/UpdateBudget2/UpdateBudget2/UnitTests/ArbitraryExceptionsUnitTest
+    Import-Module /home/runner/work/UpdateBudget2/UpdateBudget2/UnitTests/ArbitraryExceptionsUnitTest
 
     $verifiedExpenses = Import-Csv $PSScriptRoot/UnitTests/ArbitraryExceptions/verifiedExpenses.csv
 
